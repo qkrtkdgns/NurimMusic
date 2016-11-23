de37f9da6e978ba4c5c661e0f21c1bbdfc47d284
SE-Kim <121608@naver.com>
2016-11-23 19:41:11 +0900

    Beans, Mapper 변경
    Product , OrderMapper, MemberMapper

--- src/nurim/jsp/model/Product.java
+++ src/nurim/jsp/model/Product.java
@@ -3,17 +3,16 @@ package nurim.jsp.model;
 public class Product {
 	private int id = 0;
 	private int documentId = 0;
-	private int pro_categoryId = 0;
-	private String pro_img = null;
-	private String pro_name = null;
-	private int pro_price = 0;
+	private int proCategoryId = 0;
+	private String proImg = null;
+	private String proName = null;
+	private int proPrice = 0;
 	private int amount = 0;
-	private int pro_hit = 0;
-	private String reg_date = null;
-	private String edit_date = null;
+	private int proHit = 0;
+	private String regDate = null;
+	private String editDate = null;
 	private String provider = null;
 	private String content = null;
-	
 	public int getId() {
 		return id;
 	}
@@ -26,29 +25,29 @@ public class Product {
 	public void setDocumentId(int documentId) {
 		this.documentId = documentId;
 	}
-	public int getPro_categoryId() {
-		return pro_categoryId;
+	public int getProCategoryId() {
+		return proCategoryId;
 	}
-	public void setPro_categoryId(int pro_categoryId) {
-		this.pro_categoryId = pro_categoryId;
+	public void setProCategoryId(int proCategoryId) {
+		this.proCategoryId = proCategoryId;
 	}
-	public String getPro_img() {
-		return pro_img;
+	public String getProImg() {
+		return proImg;
 	}
-	public void setPro_img(String pro_img) {
-		this.pro_img = pro_img;
+	public void setProImg(String proImg) {
+		this.proImg = proImg;
 	}
-	public String getPro_name() {
-		return pro_name;
+	public String getProName() {
+		return proName;
 	}
-	public void setPro_name(String pro_name) {
-		this.pro_name = pro_name;
+	public void setProName(String proName) {
+		this.proName = proName;
 	}
-	public int getPro_price() {
-		return pro_price;
+	public int getProPrice() {
+		return proPrice;
 	}
-	public void setPro_price(int pro_price) {
-		this.pro_price = pro_price;
+	public void setProPrice(int proPrice) {
+		this.proPrice = proPrice;
 	}
 	public int getAmount() {
 		return amount;
@@ -56,23 +55,23 @@ public class Product {
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
-	public int getPro_hit() {
-		return pro_hit;
+	public int getProHit() {
+		return proHit;
 	}
-	public void setPro_hit(int pro_hit) {
-		this.pro_hit = pro_hit;
+	public void setProHit(int proHit) {
+		this.proHit = proHit;
 	}
-	public String getReg_date() {
-		return reg_date;
+	public String getRegDate() {
+		return regDate;
 	}
-	public void setReg_date(String reg_date) {
-		this.reg_date = reg_date;
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
 	}
-	public String getEdit_date() {
-		return edit_date;
+	public String getEditDate() {
+		return editDate;
 	}
-	public void setEdit_date(String edit_date) {
-		this.edit_date = edit_date;
+	public void setEditDate(String editDate) {
+		this.editDate = editDate;
 	}
 	public String getProvider() {
 		return provider;
@@ -86,13 +85,12 @@ public class Product {
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
 	@Override
 	public String toString() {
-		return "Product [id=" + id + ", documentId=" + documentId + ", pro_categoryId=" + pro_categoryId + ", pro_img="
-				+ pro_img + ", pro_name=" + pro_name + ", pro_price=" + pro_price + ", amount=" + amount + ", pro_hit="
-				+ pro_hit + ", reg_date=" + reg_date + ", edit_date=" + edit_date + ", provider=" + provider
-				+ ", content=" + content + "]";
+		return "Product [id=" + id + ", documentId=" + documentId + ", proCategoryId=" + proCategoryId + ", proImg="
+				+ proImg + ", proName=" + proName + ", proPrice=" + proPrice + ", amount=" + amount + ", proHit="
+				+ proHit + ", regDate=" + regDate + ", editDate=" + editDate + ", provider=" + provider + ", content="
+				+ content + "]";
 	}
 	
 }
