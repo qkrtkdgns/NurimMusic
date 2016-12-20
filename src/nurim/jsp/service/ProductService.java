ec0144f415d1349a1453135ba1c2cd779c2a8dd3
qkrtkdgns <dbghrtn@naver.com>
2016-12-20 11:43:01 +0900

    productService 수정

--- src/nurim/jsp/service/ProductService.java
+++ src/nurim/jsp/service/ProductService.java
@@ -2,14 +2,12 @@ package nurim.jsp.service;
 
 import java.util.List;
 
-import nurim.jsp.model.Basket;
 import nurim.jsp.model.Product;
 
 public interface ProductService {
 	
 	public int selectProductCategoryCount(Product product) throws Exception;
 	
-	public List<Product> selectProductList(Product product) throws Exception;
 	
 	/**
 	 * 카테고리별 상품 목록을 조회한다.
@@ -49,6 +47,7 @@ public interface ProductService {
 	 */
 	public List<Product> selectProductKorNewItemList() throws Exception;
 
+
 	/**
 	 * 해외 음반 index 리스트 보여주기
 	 * @return
@@ -57,11 +56,20 @@ public interface ProductService {
 	public List<Product> selectProductForeignNewItemList() throws Exception;
 
 	/**
+	 * 추천 상품 리스트
+	 * @param product - 상품 데이터
+	 * @return List - 게시물 목록
+	 * @throws Exception
+	 */
+	public List<Product> selectProductShoppingNewItemList(Product product) throws Exception;
+	
+
+	/**
 	 * 상품 음반 index HOTItem 리스트 보여주기
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Product> selectProductHOTItemList() throws Exception;
+	public List<Product> selectProductHOTItemList(Product product) throws Exception;
 	
 	/**
 	 * 단일 상품을 조회한다.
@@ -90,6 +98,22 @@ public interface ProductService {
 	 * @param product - 상품 데이터
 	 * @throws Exception
 	 */
-	public void updateProduct(Product product) throws Exception;
+	public void updateProductHit(Product product) throws Exception;
+	
+	/**
+	 * 검색된 상품 수 조회
+	 * @param  product - 상품 데이터
+	 * @return int - 상품 수
+	 * @throws Exception
+	 */
+	public int  selectProductSearchListCount(Product product) throws Exception;
+	
+	/**
+	 * 전체 목록중 검색 조건에 따라 상품을 조회한다.
+	 * @param product - 상품 데이터
+	 * @return List - 검색된 상품 목록
+	 * @throws Exception
+	 */
+	public List<Product> selectProductSearchList(Product product) throws Exception;
 	
 }
